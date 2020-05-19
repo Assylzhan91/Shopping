c791b0fa2fc2103b309075e394cce131b66fd3b3
Assylzhan91 <asylzhan.baimuratov@gmail.com>
2020-05-19 20:04:20 +0600

    part-09 finished

--- src/mocks/index.js
+++ src/mocks/index.js
@@ -1,12 +1,12 @@
 import * as R from "ramda"
+import axios from "axios"
+
 import phones from "mocks/phones"
 import categories from "mocks/categories"
 
 export async function fetchPhones() {
-  return new Promise((resolve, reject) =>{
-    resolve(phones)
-    // reject('err') 
-  })
+  const getData =  await axios.get('http://www.mocky.io/v2/5ec3e45c300000e4b039c511')
+  return  JSON.parse(getData.phones)
 }
 
 export async function loadMorePhones() {
